5ae39375ab0264672a6ee288b3426c118bd104dd
saad hafa <saad.811@icloud.com>
2019-07-05 16:18:20 +0000

    clean code up

--- src/MessageList.js
+++ src/MessageList.js
@@ -3,26 +3,41 @@ import Message from './Message';
 import Notification from './Notification';
 
 class MessageList extends Component {
+
   constructor(props){
+
     super(props);
+
   }
   render() {
+
   const messages = this.props.messages.map((message, index) =>{
+
     if(message.type === "incomingMessage"){
+
       return(
+
         <Message message={message} key={message.id}/>
+
       )
     }else if(message.type === "incomingNotification"){
+
       return (
+
         <Notification message={message} key={message.id} />
+
       )
     }
     
   } )
     return (
+
         <div>
+
             {messages}
+
         </div>
+        
     );
   }
 }
--- src/Notification.js
+++ src/Notification.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 
  class Notification extends Component {
+
     render() {
+
         return (
+
             <div>
+
                 <span style={{color: 'red'}} className="message-system">{this.props.message.content}</span>
+
             </div>
         )
     }
 }
 
-export default Notification
+export default Notification;
